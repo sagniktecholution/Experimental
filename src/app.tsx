63d149c656ca1c92d518fd8de98500901f3ff643
Bhawani Shankar Bharti <bhawani.bharti@techolution.com>
2024-07-11 22:43:05 +0530

    feat(test): add button click count test case

--- src/app.tsx
+++ src/app.tsx
@@ -18,7 +18,7 @@ export function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button role="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
